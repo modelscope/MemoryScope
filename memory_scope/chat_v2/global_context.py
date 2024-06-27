a644db8862704d323820db25a1592f0f865ace1a
jinli.yl <jinli.yl@alibaba-inc.com>
2024-06-27 13:11:21 +0800

    [dev] modify g context attr

--- memory_scope/chat_v2/global_context.py
+++ memory_scope/chat_v2/global_context.py
@@ -1,33 +1,26 @@
 from concurrent.futures import ThreadPoolExecutor
 from typing import Dict, Any
 
-import pydantic
-
 from memory_scope.chat_v2.base_memory_chat import BaseMemoryChat
 from memory_scope.enumeration.language_enum import LanguageEnum
 from memory_scope.memory.service.base_memory_service import BaseMemoryService
 from memory_scope.models.base_model import BaseModel
 from memory_scope.storage.base_monitor import BaseMonitor
 from memory_scope.storage.base_vector_store import BaseVectorStore
-from memory_scope.memory.worker.base_worker import BaseWorker
 
 
 class GlobalContext(object):
     def __init__(self):
-        self.global_configs: Dict[str, Any] = {}
+        self.global_config: Dict[str, Any] = {}
+        self.worker_config: Dict[str, Any] = {}
 
-        self.worker_config: Dict[str, Dict[str, BaseWorker]] = {}
-
+        self.memory_service_dict: Dict[str, BaseMemoryService] = {}
         self.model_dict: Dict[str, BaseModel] = {}
-
         self.memory_chat_dict: Dict[str, BaseMemoryChat] = {}
 
         self.vector_store: BaseVectorStore | None = None
-
         self.monitor: BaseMonitor | None = None
-
         self.thread_pool: ThreadPoolExecutor | None = None
-
         self.language: LanguageEnum = LanguageEnum.EN
 
 
